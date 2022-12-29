72d97d4398d357d57e72a83985e947b6e9150ba5
Bbullinger <Brandon.Bullinger@Yahoo.com>
2022-12-29 07:39:23 -0500

    stuck on foreign key ref

--- app.py
+++ app.py
@@ -2,14 +2,13 @@
 
 from flask import Flask, request, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "my_password"
 
-app.config["SQLALCHEMY_ECHO"] = True
 
 toolbar = DebugToolbarExtension(app)
 
@@ -81,5 +80,25 @@ def edit_user_post(user_id):
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
+    db.session.commit()
+    return redirect("/users")
+
+
+@app.route("/users/<int:user_id>/new_post")
+def add_post_get(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template("new_post.html", user=user)
+
+
+@app.route("/users/<int:user_id>/new_post", methods=["POST"])
+def add_post_post(user_id):
+    if not request.form["title"] or not request.form["content"]:
+        flash("Title and Content required.")
+        return redirect(f"/users/{user_id}/new_post")
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form["title"], content=request.form["content"], user_id=user
+    )
+    db.session.add(new_post)
     db.session.commit()
     return redirect("/users")
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -17,12 +17,33 @@ class User(db.Model):
     last_name = db.Column(db.Text, nullable=False, unique=True)
     image_url = db.Column(db.Text, default=default_pic)
 
+    posts = db.relationship("Post", backref="users")
+
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
     def __repr__(self):
         s = self
         return f"Class:User First Name: {s.first_name} Last Name: {s.last_name} Image URL: {s.image_url}"
+
+
+class Post(db.Model):
+    """Post"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.Text, nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(
+        # datetime grabs current date and time, and turns it into a human friendly format
+        db.Text,
+        default=datetime.now().strftime("%Y/%M/%D, %H:%M:%S"),
+    )
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"{self.poster} said {self.content} at {self.created_at}"
 
 
 def connect_db(app):
